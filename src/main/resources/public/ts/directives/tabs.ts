149029efdebedc5c18e1c7257cb7318a4d6e4e81
Jinhda Douang <jinhda.douangphrachanh@epitech.eu>
2021-04-01 09:40:28 +0200

    fix(infra-front 3.12): MLYSTOR-514 use auto $digest on tabs switch (#52)
    Co-authored-by: jinhda douang <s>

--- src/main/resources/public/ts/directives/tabs.ts
+++ src/main/resources/public/ts/directives/tabs.ts
@@ -1,6 +1,6 @@
 import {ng, idiom as lang} from 'entcore';
 
-export const Tabs = ng.directive('tabs', () => {
+export const Tabs = ng.directive('tabs', ($timeout) => {
     return {
         restrict: 'E',
         scope: {
@@ -10,20 +10,18 @@ export const Tabs = ng.directive('tabs', () => {
         },
         template: `
         <div class="tabs-container row">
-            <div ng-repeat="menu  in menus" class="menu aligned" ng-class="{'selected': ngModel == menu.value}" ng-click="selectMenu(menu)">
+            <div ng-repeat="menu in menus" class="menu aligned" ng-class="{'selected': ngModel == menu.value}" ng-click="selectMenu(menu)">
                 [[lang.translate(menu.name)]]
             </div>
         </div>
         `,
         link: function ($scope, $element, $attrs, ngModel) {
             $scope.lang = lang;
-            $scope.selectMenu =(menu)=>{
+            $scope.selectMenu = (menu) => {
                 let oldModel = $scope.ngModel;
                 $scope.ngModel = menu.value;
-                if(oldModel != $scope.ngModel && $scope.ngChange){
-                    $scope.$apply();
-                    $scope.ngChange();
-
+                if (oldModel != $scope.ngModel && $scope.ngChange) {
+                    $timeout($scope.ngChange);
                 }
             }
         }
