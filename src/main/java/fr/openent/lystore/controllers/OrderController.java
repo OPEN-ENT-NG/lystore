bca9852c0d53cc91c8e5fb46fcf7eed75db9a4a9
rseuret <remy.seuret@cgi.com>
2022-03-03 08:08:59 +0100

    no longer cast email sender each time api is called

--- src/main/java/fr/openent/lystore/controllers/OrderController.java
+++ src/main/java/fr/openent/lystore/controllers/OrderController.java
@@ -59,7 +59,8 @@ public class OrderController extends ControllerHelper {
     private AgentService agentService;
     private ProgramService programService;
     private ExportService exportService;
-
+    LystoreEmailFactoryHelper notificationHelpDeskEmailFactory;
+    LystoreEmailFactoryHelper notificationEmailFactory;
     public static final String UTF8_BOM = "\uFEFF";
 
     private static DecimalFormat decimals = new DecimalFormat("0.00");
@@ -76,6 +77,12 @@ public class OrderController extends ControllerHelper {
         this.agentService = new DefaultAgentService(Lystore.lystoreSchema, "agent");
         this.programService = new DefaultProgramService(Lystore.lystoreSchema, "program");
         exportService = new DefaultExportServiceService(storage);
+
+        String emailNotificationHelpDeskSender = config.getJsonObject("mail",new JsonObject()).getString("notificationHelpDeskMail","cesame.lystore@monlycee.net");
+        this.notificationHelpDeskEmailFactory = new LystoreEmailFactoryHelper(vertx, config,emailNotificationHelpDeskSender);
+        String emailNotificationSender = config.getJsonObject("mail",new JsonObject()).getString("notificationMail","ne-pas-repondre@ent.iledefrance.fr");
+        this.notificationEmailFactory = new LystoreEmailFactoryHelper(vertx, config,emailNotificationSender);
+
     }
 
     @Get("/orders/:idCampaign/:idStructure")
@@ -1209,10 +1216,7 @@ public class OrderController extends ControllerHelper {
             final String orderNumber = order.getString("bc_number");
             try{
                String domainMail =  config.getJsonObject("mail").getString("domainMail");
-                String emailNotificationSender = config.getJsonObject("mail",new JsonObject()).getString("notificationMail","ne-pas-repondre@ent.iledefrance.fr");
-                LystoreEmailFactoryHelper emailFactory = new LystoreEmailFactoryHelper(vertx, config,emailNotificationSender);
-
-                EmailSender emailSender = emailFactory.getSender();
+                EmailSender emailSender = notificationEmailFactory.getSender();
                 orderService.sendNotification(orderNumber,domainMail,request,emailSender);
             }catch (Exception e){
                 badRequest(request,e.getMessage());
@@ -1231,12 +1235,10 @@ public class OrderController extends ControllerHelper {
             final String orderNumber = order.getString("bc_number");
             try{
                 String domainMail =  config.getJsonObject("mail").getString("domainMail","lystore.monlycee.net");
-                String emailNotificationHelpDeskSender = config.getJsonObject("mail",new JsonObject()).getString("notificationHelpDeskMail","cesame.lystore@monlycee.net");
                 String emailNotificationHelpDeskReceiver =
                         config.getJsonObject("mail",new JsonObject()).getString("notificationHelpDeskReceiver","collecteur.lystore@monlycee.net");
-                LystoreEmailFactoryHelper emailFactory = new LystoreEmailFactoryHelper(vertx, config,emailNotificationHelpDeskSender);
 
-                EmailSender emailSender = emailFactory.getSender();
+                EmailSender emailSender = notificationHelpDeskEmailFactory.getSender();
                 orderService.sendNotificationHelpDesk(orderNumber,domainMail,request,emailSender,emailNotificationHelpDeskReceiver);
             }catch (Exception e){
                 badRequest(request,e.getMessage());
