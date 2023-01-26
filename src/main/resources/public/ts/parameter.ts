861b03962c66b2d2e4caede80f0d7d4a00616c1d
rseuret <remy.seuret@cgi.com>
2023-01-26 14:09:38 +0100

    end style

--- src/main/resources/public/ts/parameter.ts
+++ src/main/resources/public/ts/parameter.ts
@@ -1,10 +1,15 @@
 import {ng, routes} from "entcore";
 import {ParameterService} from "./services";
 import {activeStructureController,parameterMainController} from "./controllers/index"
+import * as directives from "./directives";
 
 ng.services.push(ParameterService);
 ng.controllers.push(activeStructureController);
 ng.controllers.push(parameterMainController);
+
+for (let directive in directives) {
+    ng.directives.push(directives[directive]);
+}
 
 routes.define(($routeProvider) => {
     $routeProvider
